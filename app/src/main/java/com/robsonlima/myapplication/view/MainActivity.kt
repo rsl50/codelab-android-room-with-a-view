93f6a1e3a9957f72094e826f94fc8542c223534d
Robson Lima <rsl.robson@gmail.com>
2022-10-20 18:53:52 -0300

    Adding the RecyclerView.

---
+++ app/src/main/java/com/robsonlima/myapplication/view/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.robsonlima.myapplication.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.robsonlima.myapplication.R
+import com.robsonlima.myapplication.adapter.WordListAdapter
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+}
