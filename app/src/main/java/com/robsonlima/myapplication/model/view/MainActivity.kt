ccea7d2d3b7c8141621582c470018b1ad5392ea5
Robson Lima <rsl.robson@gmail.com>
2022-10-20 18:29:27 -0300

    Adding the ViewModel to the project.

--- app/src/main/java/com/robsonlima/myapplication/model/view/MainActivity.kt
+++ app/src/main/java/com/robsonlima/myapplication/model/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.robsonlima.myapplication
+package com.robsonlima.myapplication.model.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.robsonlima.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
